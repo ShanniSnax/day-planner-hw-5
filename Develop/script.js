c3adf01125c36b4a7b80da5df7c708eea009ff77
Snax <shanni.c.suissa@gmail.com>
2020-11-25 11:58:37 -0600

    added in the style changes for past/present/future, and finished the local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,31 @@
-$(".saveBtn").on("click", function(){
-    var plan = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-    localStorage.setItem(time, plan);
-    console.log(time);
-    console.log(plan);
+$(".saveBtn").on("click", function () {
+  var plan = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+  localStorage.setItem(time, plan);
+});
 
-})
+for (i = 9; i <= 18; i++) {
+  $(`#${i} .description`).val(localStorage.getItem(i));
+}
 
+function colorUpdate() {
+  var currTime = moment().hours();
+  $(".time-block").each(function () {
+    var time = parseInt($(this).attr("id"));
 
-$("#12 .description").val(localStorage.getItem("12"))
+    if (currTime === time) {
+      $(this).addClass("present");
+      $(this).removeClass("past");
+    } else if (currTime < time) {
+      $(this).addClass("past");
+    } else {
+      $(this).addClass("future");
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+    }
+  });
+} 
+
+var timeInterval = setInterval(colorUpdate, 60000);
+
+colorUpdate();
